888979bb51a67215707220d7a3f8566f1c5a2de2
DonalHowe <donalhowe@gmail.com>
2022-10-22 22:59:38 +0100

    template

---
+++ fyp/fyp/Cell.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+class Cell
+{
+
+public:
+
+};
+
---
+++ fyp/fyp/ScreenSize.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+class ScreenSize {
+public:
+	static const int M_HEIGHT = 800;
+	static const int M_WIDTH = 800;
+};
